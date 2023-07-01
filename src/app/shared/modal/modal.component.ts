97a4f263f4ac5ea8a218524630f1b535fa2ac3c0
Chandan Thakur <thakurchandan4562@gmail.com>
2023-07-01 15:50:41 +0545

    Tabs Completed

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, ElementRef, OnInit } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
+  // providers: [ModalService]
 })
-export class ModalComponent {
-  constructor(public modal: ModalService){
-    console.log(this.modal.visible)
+export class ModalComponent implements OnInit{
+  @Input() ModalId = '';
+  constructor(public modal: ModalService, public el: ElementRef){
+    
+  }
+
+  closeModal(){
+    this.modal.toogleModal(this.ModalId);
+  }
+
+  ngOnInit(): void{
+    document.body.appendChild(this.el.nativeElement)
   }
 
 }
